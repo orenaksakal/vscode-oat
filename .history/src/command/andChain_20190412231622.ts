352deb961cc24c8414902aa0311c4b4ebcfe2c34
Oren Aksakal <orenaksakal@gmail.com>
2019-04-13 09:13:04 +0200

    Revert "remove ,history"
    This reverts commit 86010779ab6f91c0b6830caa58fcdf1f856ff9e2.

---
+++ .history/src/command/andChain_20190412231622.ts
@@ -0,0 +1,61 @@
+import * as vscode from 'vscode';
+
+function transformToAndChain(diagnosticCollection: vscode.DiagnosticCollection) {
+    return vscode.commands.registerCommand(
+        'stringtransformer.transformToAndChain',
+        () => {
+            diagnosticCollection.clear();
+
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                vscode.window.showWarningMessage(
+                    'You can only use extension within a document'
+                );
+                return;
+            }
+            const selection = editor.selection;
+
+            const selectedRange = new vscode.Range(
+                selection.start,
+                selection.end
+            );
+
+            const str = editor.document.getText(selectedRange);
+
+            function parse(text: String) {
+                let container: Array<String> = [];
+
+                text.split(/(\.|\(.*?\)|\[.*?\])/g).forEach((item, index) => {
+                    if (index === 0) {
+                        container.push(item);
+                    } else if (item !== '') {
+                        container.push(container.slice(-1, index) + item);
+                    }
+                });
+
+                return container
+                    .filter(el => {
+                        return el.slice(-1) !== '.';
+                    })
+                    .join(' && \n');
+            }
+
+            try {
+                const result = parse(str);
+                editor
+                    .edit((editBuilder: vscode.TextEditorEdit) => {
+                        editBuilder.replace(selection, result);
+                    })
+                    .then(() => editor.document.save());
+            } catch (error) {
+                const diagnostic = new vscode.Diagnostic(
+                    selectedRange,
+                    `[vscode-oat]: ${error.message}`,
+                    vscode.DiagnosticSeverity.Error
+                );
+                diagnosticCollection.set(editor.document.uri, [diagnostic]);
+            }
+        }
+    );
+}
+export default transformToAndChain;
