929b794fa4e1eed701a0c6d0c3fe033558e2cb0a
orenaksakal <orenaksakal@gmail.com>
2019-10-27 14:02:30 +0100

    fix formatting

--- src/command/andChain.ts
+++ src/command/andChain.ts
@@ -1,53 +1,44 @@
 import * as vscode from 'vscode';
 
 function transformToAndChain(diagnosticCollection: vscode.DiagnosticCollection) {
-    return vscode.commands.registerCommand(
-        'vscode-oat.transformToAndChain',
-        () => {
-            diagnosticCollection.clear();
+    return vscode.commands.registerCommand('vscode-oat.transformToAndChain', () => {
+        diagnosticCollection.clear();
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor.selection;
+        const selectedRange = new vscode.Range(
+            selection.start,
+            selection.end
+        );
+        const str = editor.document.getText(selectedRange);
 
-            const editor = vscode.window.activeTextEditor;
-            if (!editor) {
-                vscode.window.showWarningMessage(
-                    'You can only use vscode-oat within a document'
-                );
-                return;
-            }
-            const selection = editor.selection;
-            const selectedRange = new vscode.Range(
-                selection.start,
-                selection.end
+        function parse(text: String) {
+            let container: Array<String> = [];
+
+            text.split(/(\.|\(.*?\)|\[.*?\])/g).forEach((item, index) => {
+                if (index === 0) {
+                    container.push(item);
+                } else if (item !== '') {
+                    container.push(container.slice(-1, index) + item);
+                }
+            });
+
+            return container.filter(el => el.slice(-1) !== '.').join(' && ');
+        }
+
+        if (!editor) {
+            vscode.window.showWarningMessage(
+                'You can only use vscode-oat within a document'
             );
-            const str = editor.document.getText(selectedRange);
 
-            function parse(text: String) {
-                let container: Array<String> = [];
-
-                text.split(/(\.|\(.*?\)|\[.*?\])/g).forEach((item, index) => {
-                    if (index === 0) {
-                        container.push(item);
-                    } else if (item !== '') {
-                        container.push(container.slice(-1, index) + item);
-                    }
-                });
-
-                return container.filter(el => el.slice(-1) !== '.').join(' && ');
-            }
-
+        } else {
             try {
-                const result = parse(str);
-                editor
-                    .edit((editBuilder: vscode.TextEditorEdit) => editBuilder.replace(selection, result))
-                    .then(() => editor.document.save());
+                editor.edit((editBuilder: vscode.TextEditorEdit) => editBuilder.replace(selection, parse(str))).then(() => editor.document.save());
             } catch (error) {
-                const diagnostic = new vscode.Diagnostic(
-                    selectedRange,
-                    `[vscode-oat]: ${error.message}`,
-                    vscode.DiagnosticSeverity.Error
-                );
+                const diagnostic = new vscode.Diagnostic(selectedRange, `[vscode-oat]: ${error.message}`, vscode.DiagnosticSeverity.Error);
                 diagnosticCollection.set(editor.document.uri, [diagnostic]);
             }
         }
+    }
     );
 }
 export default transformToAndChain;
